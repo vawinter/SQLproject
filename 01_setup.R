2ae3b8e66c2a751594123a53092a408fe12529f8
vermanica <veronicaann621@gmail.com>
2022-11-28 16:42:46 -0500

    troubleshooting functions in the RODBC package

--- 01_setup.R
+++ 01_setup.R
@@ -2,3 +2,14 @@
 
 # Install package
 install.packages("RODBC")
+
+library(RODBC)
+
+# Set up driver info and database path
+DRIVERINFO <- "Driver={Microsoft Access Driver (*.accdb)};"
+MDBPATH <- "ERMN_Fish_20221101.accdb"
+PATH <- paste0(DRIVERINFO, "DBQ=", MDBPATH)
+
+##Read in tables from Access database 
+db <- file.path("ERMN_Fish_20221101.accdb")
+ch <- odbcDriverConnect(db)
