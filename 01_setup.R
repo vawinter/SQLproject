120c75fe880bc3805fea503ffbc7b7a985021ca6
Stum <mbs6798@psu.edu>
2022-11-29 13:41:40 -0500

    Connecting microsoft driver to R. Data exploration.

--- 01_setup.R
+++ 01_setup.R
@@ -1,15 +1,34 @@
+# 
+# 
+# # Install package
+# install.packages("RODBC")
+# 
+# library(RODBC)
+# 
+# # Set up driver info and database path
+# DRIVERINFO <- "Driver={Microsoft Access Driver (*.accdb)};"
+# MDBPATH <- "ERMN_Fish_20221101.accdb"
+# PATH <- paste0(DRIVERINFO, "DBQ=", MDBPATH)
+# 
+# ##Read in tables from Access database 
+# db <- file.path("ERMN_Fish_20221101.accdb")
+# ch <- odbcDriverConnect(db)
 
-
-# Install package
-install.packages("RODBC")
-
+#call library
 library(RODBC)
+#establish connection to Microsoft driver that you created
+con <- odbcConnect("ERMNSQL")
 
-# Set up driver info and database path
-DRIVERINFO <- "Driver={Microsoft Access Driver (*.accdb)};"
-MDBPATH <- "ERMN_Fish_20221101.accdb"
-PATH <- paste0(DRIVERINFO, "DBQ=", MDBPATH)
+#view all tables
+sqlTables(con, tableType = "TABLE")$TABLE_NAME
 
-##Read in tables from Access database 
-db <- file.path("ERMN_Fish_20221101.accdb")
-ch <- odbcDriverConnect(db)
+#view column names in a table
+sqlColumns(con, "tbl_Events")$COLUMN_NAME
+sqlColumns(con, "tbl_Sites")$COLUMN_NAME
+sqlColumns(con, "tbl_Fish")$COLUMN_NAME
+sqlColumns(con, "tbl_Fish_Count")$COLUMN_NAME
+sqlColumns(con, "tbl_Habitat_Transect_Fields")$COLUMN_NAME
+sqlColumns(con, "tbl_Habitat_Transect")$COLUMN_NAME
+sqlColumns(con, "tbl_Habitat")$COLUMN_NAME
+
+#tlu tables have metadata
