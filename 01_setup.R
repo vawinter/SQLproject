06d43a6b6168be88d324d14c02af56236277bf9b
Stum <mbs6798@psu.edu>
2022-11-30 15:47:30 -0500

    queries

--- 01_setup.R
+++ 01_setup.R
@@ -21,8 +21,6 @@ con <- odbcConnect("ERMNSQL")
 #view all tables
 sqlTables(con, tableType = "TABLE")$TABLE_NAME
 
-# Set up driver info and database path
-con <- odbcConnect("programSQL")
 
 #view column names in a table
 sqlColumns(con, "tbl_Events")$COLUMN_NAME
@@ -31,7 +29,19 @@ sqlColumns(con, "tbl_Fish")$COLUMN_NAME
 sqlColumns(con, "tbl_Fish_Count")$COLUMN_NAME
 sqlColumns(con, "tbl_Habitat_Transect_Fields")$COLUMN_NAME
 sqlColumns(con, "tbl_Habitat_Transect")$COLUMN_NAME
-sqlColumns(con, "tbl_Habitat")$COLUMN_NAME
+habitat <- sqlColumns(con, "tbl_Habitat")
 
 #tlu tables have metadata
+
+WQqry <-  "SELECT Event_ID, Site_ID, Start_Date, Visit_Number, WQ_Time, Temp_C,
+        SpecficConductance, DO_mgl, DO_Percent, pH, WaterColor, Secchi_Depth_cm 
+        FROM  tbl_Events"
+
+SiteWQ <- sqlQuery(con, WQqry)
+
+nameqry <- "SELECT Site_ID, Site_Name From tbl_Sites"
+SiteNames <- sqlQuery(con, nameqry)
+
+site_WQ <- merge(SiteNames, SiteWQ, by="Site_ID")
+
 
